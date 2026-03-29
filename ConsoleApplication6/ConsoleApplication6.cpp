a53f82c2c3028f11a40fe54225ca3a71ebbc016f
Admin <Admin@DESKTOP-Q79K2ON>
2026-03-29 12:27:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+    setlocale(LC_ALL, "");
+    double x, n;
+
+    std::cout << "Введите число x: ";
+    std::cin >> x;
+    std::cout << "Введите шаг n: ";
+    std::cin >> n;
+
+    if (n == 0) {
+        std::cout << "Ошибка: шаг не может быть равен 0" << std::endl;
+        return 1;
+    }
+
+    double result = round(x / n) * n;
+
+    std::cout << "Ближайшее число, кратное " << n << ": " << result << std::endl;
+
+    return 0;
+}
